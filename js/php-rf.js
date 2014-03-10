ddc80f98ee3e456fcfebcd069f83535ae5d716fc
Elvis Ciotti <elvis@phpntips.com>
2014-03-10 11:50:02 +0000

    code format

--- js/php-rf.js
+++ js/php-rf.js
@@ -1,28 +1,29 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-	chrome.tabs.executeScript(null,
-                       {file:"data/data.js"});
-
-	randomFunction(data);
+    chrome.tabs.executeScript(
+        null,
+        {file: "data/data.js"}
+    );
+    randomFunction(data);
 });
 
 // random PHP function
-function randomFunction(data){
+function randomFunction(data)
+{
+    // data is an json array defined in an external file
+    var obj = $.parseJSON(data);
 
-		// data is an json array defined in an external file
-		var obj		= $.parseJSON(data);
-		
-		// get the PHP version
-		$.each( obj, function( key) {
-			ver = key;
-		});
+    // get the PHP version
+    $.each(obj, function(key) {
+        ver = key;
+    });
 
-		var min		= 0;
-		var max		= (obj[ver].length - 1);
+    var min = 0;
+    var max = (obj[ver].length - 1);
 
-		var curr	= Math.floor(Math.random() * (max - min + 1)) + min;
+    var curr = Math.floor(Math.random() * (max - min + 1)) + min;
 
-		// open the new page
-		chrome.tabs.executeScript(null, {code:"window.location = 'http://www.php.net/" + obj[ver][curr] + "';"});
-		window.close();
+    // open the new page
+    chrome.tabs.executeScript(null, {code: "window.location = 'http://www.php.net/" + obj[ver][curr] + "';"});
+    window.close();
 }
