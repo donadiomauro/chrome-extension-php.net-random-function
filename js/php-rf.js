72e608d025a8c20653db8153ceb28d04274e635f
Elvis Ciotti <elvis@phpntips.com>
2014-03-10 11:56:08 +0000

    change function order + more code comments

--- js/php-rf.js
+++ js/php-rf.js
@@ -1,18 +1,10 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-
-    chrome.tabs.executeScript(
-        null,
-        {file: "data/data.js"}
-    );
-    randomFunction(data);
-});
-
 // random PHP function
+// open http://www.php.net/<path> where <path> is a random value of the `data`array
 function randomFunction(data)
 {
     // data is an json array defined in an external file
     var obj = $.parseJSON(data);
-
+    
     // get the PHP version
     $.each(obj, function(key) {
         ver = key;
@@ -27,3 +19,12 @@ function randomFunction(data)
     chrome.tabs.executeScript(null, {code: "window.location = 'http://www.php.net/" + obj[ver][curr] + "';"});
     window.close();
 }
+
+// listener
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(
+        null,
+        {file: "data/data.js"}
+    );
+    randomFunction(data);
+});
