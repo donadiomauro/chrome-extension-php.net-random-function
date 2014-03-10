102a468577336d7bee8b402e9d1f4f3ceeb94fea
Elvis Ciotti <elvis@phpntips.com>
2014-03-10 12:29:08 +0000

    open link into the current tab, otherwise not working with blank tab

--- js/php-rf.js
+++ js/php-rf.js
@@ -1,4 +1,12 @@
-// random PHP function
+// open a link in the current window
+function openLink(href)
+{
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        chrome.tabs.update(tab.id, {url: href});
+    });
+}
+
 // open http://www.php.net/<path> where <path> is a random value of the `data`array
 function randomFunction(data)
 {
@@ -13,12 +21,11 @@ function randomFunction(data)
     var min = 0;
     var max = (obj[ver].length - 1);
 
-    var curr = Math.floor(Math.random() * (max - min + 1)) + min;
+    var randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    // open the new page
-    chrome.tabs.executeScript(null, {code: "window.location = 'http://www.php.net/" + obj[ver][curr] + "';"});
-    window.close();
+    openLink("http://www.php.net/" + obj[ver][randomIndex]);
 }
+
 
 // listener
 chrome.browserAction.onClicked.addListener(function(tab) {
